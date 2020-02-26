5efff8c9ec1f0358f184c0cac74ec909e3b55a54
Dakota Willingham <kota@vaga.tk>
2020-02-26 04:24:53 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -1,7 +1,18 @@
+require 'pry'
+
 def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  item_counter = 0
+  while item_counter < collection.length do
+    if name == collection[item_counter][:item]
+      return collection[item_counter]
+    end
+    item_counter += 1
+  end
+  nil
+  # binding.pry
 end
 
 def consolidate_cart(cart)
@@ -9,6 +20,18 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  consolidated = []
+  cart_counter = 0
+  while cart_counter < cart.length do
+    if !consolidated[cart[cart_counter]]
+      consolidated << cart[cart_counter]
+      consolidated[cart_counter][:count] = 1
+    else
+      consolidated[cart_counter][:count] += 1
+    end
+    cart_counter += 1
+  end
+  binding.pry
 end
 
 def apply_coupons(cart, coupons)
